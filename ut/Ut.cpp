a69eb778b37b27f429d122b01be15b21f92abae1
Tomasz Piecha <tomasz.piecha@nokia.com>
2019-05-15 12:09:38 +0200

    init commit

---
+++ ut/Ut.cpp
@@ -0,0 +1,22 @@
+//
+// Created by tpiecha on 15.05.2019.
+//
+
+#ifndef REFACTORING_UT_CPP
+#define REFACTORING_UT_CPP
+
+#include <gtest/gtest.h>
+
+class RefactorTest : public ::testing::Test
+{
+
+};
+
+TEST(RefactorTest, test1)
+{
+  EXPECT_TRUE(true);
+}
+
+
+
+#endif //REFACTORING_UT_CPP
